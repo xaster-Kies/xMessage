9f57043155cff4d2578678d72ec97407e7a9cc6e
TiokengSamuel <samueledward40@gmail.com>
2021-01-19 22:31:51 +0100

    sidebar chat

--- xmessage/src/Sidebar.js
+++ xmessage/src/Sidebar.js
@@ -24,6 +24,10 @@ function Sidebar() {
 
             <div className="sidebar__chats">
                 <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
             </div>
         </div>
     )
