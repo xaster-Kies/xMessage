5c23c3b11bf868757ba2744eefdfa50c322b0b61
TiokengSamuel <samueledward40@gmail.com>
2021-01-19 19:40:00 +0100

    material ui

--- xmessage/src/Sidebar.js
+++ xmessage/src/Sidebar.js
@@ -3,8 +3,16 @@ import './Sidebar.css'
 
 function Sidebar() {
     return (
-        <div>
-            
+        <div className="sidebar">
+            <h2>I am a sidebar</h2>
+
+            <div className="sidebar__header">
+
+            </div>
+
+            <div className="sidebar__chats">
+                
+            </div>
         </div>
     )
 }
