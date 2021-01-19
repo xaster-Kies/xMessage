5108b7d2f34194e03e0affc1f84194d31c38547d
TiokengSamuel <samueledward40@gmail.com>
2021-01-19 21:42:42 +0100

    sidebar chat

--- xmessage/src/Sidebar.js
+++ xmessage/src/Sidebar.js
@@ -22,7 +22,7 @@ function Sidebar() {
             </div>
 
             <div className="sidebar__chats">
-
+                <SidebarChat/>
             </div>
         </div>
     )
---
+++ xmessage/src/SidebarChat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SidebarChat() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SidebarChat
