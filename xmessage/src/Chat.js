0c990786f738805b133aeac7ce02ad174a6e0a77
TiokengSamuel <samueledward40@gmail.com>
2021-01-19 22:56:14 +0100

    chat header

--- xmessage/src/Chat.js
+++ xmessage/src/Chat.js
@@ -7,6 +7,7 @@ function Chat() {
             {/* Chat header */}
             <div className="chat__header">
                 <h4>To: Channel Name</h4>
+                <strong>Details</strong>
             </div>
 
             {/* Chat messages */}
