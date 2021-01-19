410e24b2dfc7353a1de8fa752e00b809b6938f81
TiokengSamuel <samueledward40@gmail.com>
2021-01-19 21:47:32 +0100

    sidbarchat

--- xmessage/src/SidebarChat.js
+++ xmessage/src/SidebarChat.js
@@ -6,6 +6,11 @@ function SidebarChat() {
     return (
         <div className="sidebarChat">
             <Avatar/>
+            <div className="sidbarChat__info">
+                <h3>Channel Name</h3>
+                <p>last msg sent...</p>
+                <small>timestamp</small>
+            </div>
         </div>
     )
 }
