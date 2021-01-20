5b72123525e4b5b3aaa02911eb423a9663510e16
TiokengSamuel <samueledward40@gmail.com>
2021-01-20 07:54:07 +0100

    firebase setup

---
+++ xmessage/src/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCZMw-o6fbz7zNpqpbWrcNFUjeNO0O9F28",
+    authDomain: "xmessage-2a0de.firebaseapp.com",
+    projectId: "xmessage-2a0de",
+    storageBucket: "xmessage-2a0de.appspot.com",
+    messagingSenderId: "613998007234",
+    appId: "1:613998007234:web:59a81bbf7a4b9633d6fc5b",
+    measurementId: "G-YGHZYC6B2E"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
