0b61f30dfe02871d2a0d0df19ea8da31d1e53c15
TiokengSamuel <samueledward40@gmail.com>
2021-01-21 05:51:53 +0100

    google auth working

--- xmessage/src/Login.js
+++ xmessage/src/Login.js
@@ -1,10 +1,12 @@
 import { Button } from '@material-ui/core'
 import React from 'react'
+import { auth, provider } from './firebase'
 import './Login.css'
 
 function Login() {
     const signIn = () => {
-        
+        auth.signInWithPopup(provider)
+        .catch((error) => alert(error, message));
     }
 
     return (
