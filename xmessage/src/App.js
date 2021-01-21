f19e658099465ccdb1520a8f63e22409c24bc27e
TiokengSamuel <samueledward40@gmail.com>
2021-01-21 05:26:48 +0100

    login functionality

--- xmessage/src/App.js
+++ xmessage/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
 import Xmessage from './Xmessage'
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
-      <Xmessage/>
+      {user ? <Xmessage/> : <h2>You need to login</h2>}
     </div>
   );
 }
