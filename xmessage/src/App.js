0d8a354a0ed3daf5b2ffc83c3011692a0c3f3da9
TiokengSamuel <samueledward40@gmail.com>
2021-01-21 17:29:01 +0100

    listenner for auth

--- xmessage/src/App.js
+++ xmessage/src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import './App.css';
 import { selectUser } from './features/userSlice';
 import Login from './Login';
 import Xmessage from './Xmessage'
+import { auth } from "./firebase"
 
 function App() {
   const user = useSelector(selectUser);
+
+  useEffect(() => {
+   auth.onAuthStateChanged(authUser => {
+     if(authUser){
+       //user Logged in
+    
+     }else {
+       //user is Logged out
+     }
+   })
+  }, [user])
   return (
     <div className="app">
       {user ? <Xmessage/> : <Login/>}
--- xmessage/src/Login.js
+++ xmessage/src/Login.js
@@ -11,7 +11,7 @@ function Login() {
 
     return (
         <div className="login">
-        <div className="login__logo">
+    <div className="login__logo">
             <img 
             src="https://upload.wikimedia.org/wikipedia/commons/5/56/IMessage_logo_%28Apple_Inc.%29.png" alt=""/>
             <h1>xMessage</h1>
