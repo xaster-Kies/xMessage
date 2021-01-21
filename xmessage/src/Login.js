e01f1aa2b168c92dd9685f3ec8fc9016b7e996c4
TiokengSamuel <samueledward40@gmail.com>
2021-01-21 05:42:19 +0100

    logo and sign in button

--- xmessage/src/Login.js
+++ xmessage/src/Login.js
@@ -4,7 +4,13 @@ import './Login.css'
 function Login() {
     return (
         <div className="login">
-            
+        <div className="login__logo">
+            <img 
+            src="https://upload.wikimedia.org/wikipedia/commons/5/56/IMessage_logo_%28Apple_Inc.%29.png" alt=""/>
+            <h1>xMessage</h1>
+        </div>
+
+        <Button>Sign In</Button>
         </div>
     )
 }
